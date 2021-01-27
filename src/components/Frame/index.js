95e1b85423e2335915911d78095220a21d50a096
Gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2021-01-27 13:10:54 -0300

    transformando a Frame num componente funcional

--- src/components/Frame/index.js
+++ src/components/Frame/index.js
@@ -1,29 +1,19 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Caixa} from './styles';
 
-class Frame extends React.Component {
-    constructor(props){
-        super();
-    }
-    render() {
-        
-        const width = this.props.width;
-        const height = this.props.height;
-        const imagem = this.props.imagem;
-        const nomeImagem = this.props.nomeImagem;
+const Frame = ({width, height, imagem, nomeImagem}) => {
 
-        return (
-            <Caixa width={width} height={height}>
-                {
-                    imagem && <img src={imagem} style = {{width: width, height: height}} alt="Logo"/>
-                }
-                {
-                    nomeImagem
-                }
-            </Caixa>
-        )
-    }
-    
+    return (
+        <Caixa width={width} height={height}>
+            {
+                imagem && <img src={imagem} style = {{width: width, height: height}} alt="Logo"/>
+            }
+            {
+                nomeImagem
+            }
+        </Caixa>
+    )
+        
 }
 
 export default Frame;
