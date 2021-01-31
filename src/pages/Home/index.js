f38ebb8ef866a067656e4fa6d880b1b73ba812f0
Gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2021-01-31 17:16:54 -0300

    adicionando grid de imagens; adicionando spinner que fica visivel quando imagens ainda estiverem carregando

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,6 +4,11 @@ import Frame from '../../components/Frame';
 
 import {storage} from '../../firebase';
 
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+
+import Loader from 'react-loader-spinner'
+
 // util
 // https://stackoverflow.com/questions/64708353/how-to-display-all-the-images-from-firebase-storage-in-react
 
@@ -12,6 +17,8 @@ const Home = () => {
     const valorInicial = [];
     const [urls, setUrls] = useState(valorInicial);
 
+    const [ImagesLoading, setImagesLoading] = useState(true);
+    
     const getImageUrlsFromStorage = async () => {
         let result = await storage.ref("images/").listAll();
         let allUrls = result.items.map(imageRef => imageRef.getDownloadURL());
@@ -22,6 +29,7 @@ const Home = () => {
         const allUrls = await getImageUrlsFromStorage();
         setUrls(allUrls);
         console.log("URLS: ", urls);
+        setImagesLoading(false);
     }
 
     // Similar ao componentDidMount e componentDidUpdate:
@@ -29,9 +37,45 @@ const Home = () => {
         loadImages();
     }, []);
 
+    if(ImagesLoading) {
+        return (
+            <Container>
+                <Loader
+                    type="Oval"
+                    color="#d685ff"
+                    height={100}
+                    width={100}
+                    //timeout={1000} //1 sec
+                />
+            </Container>
+        )
+    }
+
     return (
         <Container>
 
+
+{/*}
+            <GridList cellHeight='auto' cols={3}>
+                {urls.map((url, index) => (
+                    <GridListTile key={index} cols={url.cols || 1}>
+                        <Frame imagem={url}/>
+                    </GridListTile>
+                ))}
+            </GridList>
+*/}
+
+
+            <GridList cellHeight='auto' cols={3}>
+                {urls.map((url, index) => (
+                    <GridListTile key={index}>
+                        <Frame imagem={url}/>
+                    </GridListTile>
+                ))}
+            </GridList>
+            
+        {
+/*
             <Row>
                 {urls.map((url, index) => (
                     <div key={index}>  
@@ -39,6 +83,10 @@ const Home = () => {
                     </div>
                 ))}
             </Row>
+            */
+
+        }
+            
                     
         </Container>
     );
