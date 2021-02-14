c6aff5628237edf640f599893a9e478ea1f10233
Gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2021-02-14 14:38:40 -0300

    Melhorando a Home, agora aparecera uma mensagem quando nao existirem wallpapers

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,14 +12,27 @@ import COLORS from '../../styles/colors';
 
 const Home = () => {
 
-    const valorInicial = [];
-    const [urls, setUrls] = useState(valorInicial);
-
+    const initialValue = [];
+    const [urls, setUrls] = useState(initialValue);
     const [imagesLoading, setImagesLoading] = useState(true);
+    const [imagesExist, setImagesExist] = useState(false);
+
+    const storageHasImages = (data) => {
+        if(data.items.length > 0) return true;
+        return false;
+    }
     
     const getImageUrlsFromStorage = async () => {
-        let result = await storage.ref("images/").listAll();
-        let allUrls = result.items.map(imageRef => imageRef.getDownloadURL());
+        const result = await storage.ref("images/").listAll();
+
+        if(!storageHasImages(result)) {
+            console.log("No images here...");
+            return;
+        }
+        console.log("There are images!");
+        setImagesExist(true);
+
+        const allUrls = result.items.map(imageRef => imageRef.getDownloadURL());
         return Promise.all(allUrls);
     }
 
@@ -35,22 +48,30 @@ const Home = () => {
         loadImages();
     }, []);
 
+    if(imagesLoading) {
+        return (
+            <Container>
+                <CentralizedContent>
+                    <Loader
+                        type="Oval"
+                        color={COLORS.rosa}
+                        height={100}
+                        width={100}
+                        //timeout={1000} //1 sec
+                    />
+                </CentralizedContent>
+            </Container>
+        );
+    }
+
     return (
         <Container>
             <CentralizedContent>
-            
             {
-            imagesLoading ?
-            
-                <Loader
-                    type="Oval"
-                    color={COLORS.rosa}
-                    height={100}
-                    width={100}
-                    //timeout={1000} //1 sec
-                />
-            :
-                <Grid urls={urls}/>   
+                imagesExist ?
+                    <Grid urls={urls}/>  
+                :
+                    <h1>Ainda não foram adicionadas imagens.</h1>         
             }
             </CentralizedContent>
         </Container>
