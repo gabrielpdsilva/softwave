6c71a8df995975812b94c1795c53c1d9985b20fe
Gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2021-02-17 11:41:21 -0300

    agora as imagens da Home vao ser carregadas com base na categoria escolhida pelo usuario

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,6 +10,10 @@ import COLORS from '../../styles/colors';
 // util
 // https://stackoverflow.com/questions/64708353/how-to-display-all-the-images-from-firebase-storage-in-react
 
+// hooks
+// https://www.digitalocean.com/community/tutorials/react-replacing-component-lifecycles-with-useeffect
+// https://www.digitalocean.com/community/tutorials/react-hooks
+
 const Home = () => {
 
     const initialValue = [];
@@ -17,13 +21,16 @@ const Home = () => {
     const [imagesLoading, setImagesLoading] = useState(true);
     const [imagesExist, setImagesExist] = useState(false);
 
+    const [category, setCategory] = useState("cyberpunk");
+
     const storageHasImages = (data) => {
         if(data.items.length > 0) return true;
         return false;
     }
     
     const getImageUrlsFromStorage = async () => {
-        const result = await storage.ref("images/").listAll();
+        //const result = await storage.ref("images/").listAll();
+        const result = await storage.ref(`images/${category}/`).listAll();
 
         if(!storageHasImages(result)) {
             console.log("No images here...");
@@ -34,6 +41,28 @@ const Home = () => {
 
         const allUrls = result.items.map(imageRef => imageRef.getDownloadURL());
         return Promise.all(allUrls);
+
+    }
+
+    /*
+     *  funcao para pegar todas as imagens de todas as pastas. Ainda nao funcional.
+     */
+    const getAllImageUrlsFromStorage = async () => {
+
+        //https://firebase.google.com/docs/storage/web/list-files
+        //https://firebase.google.com/docs/storage/web/create-reference?hl=pt-br
+
+        const storageRef = storage.ref();
+        const listRef = storageRef.child('images/');
+
+        listRef.listAll().then(res => {
+            res.prefixes.forEach(folder => {
+                console.log("Folder: ", folder);
+                console.log("Folder's name: ", folder.name);
+                // pegando uma imagem diretamente
+                // let minhaImagem = folder.parent.child('shinji.png').getDownloadURL(); 
+            })
+        });
     }
 
     const loadImages = async () => {
@@ -43,11 +72,15 @@ const Home = () => {
         setImagesLoading(false);
     }
 
+    const handleSelect = (event) => {
+        setCategory(event.target.value);
+    }
+
     // Similar ao componentDidMount e componentDidUpdate:
     useEffect(() => {
         document.title = "SOFTWAVE";
         loadImages();
-    }, []);
+    }, [category]); // vai entrar em ação assim que a categoria mudar.
 
     if(imagesLoading) {
         return (
@@ -70,7 +103,22 @@ const Home = () => {
             <CentralizedContent>
             {
                 imagesExist ?
-                    <Grid urls={urls}/>  
+
+                <div>
+                
+                <div style={{margin: 5}}>
+                
+                    <p>{category}</p>
+
+                    <select value={category} onChange={handleSelect}>
+                        <option value="cyberpunk">Cyberpunk</option>
+                        <option value="doomerwave">Doomer Wave</option>
+                        <option value="synthwave">Synthwave</option>
+                        <option selected value="vaporwave">Vaporwave</option>
+                    </select>
+                </div>
+                    <Grid urls={urls}/> 
+                    </div>
                 :
                     <h1>Ainda não foram adicionadas imagens.</h1>         
             }
