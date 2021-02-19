5b81e389a115661c3cf83ccfdf03e33d91404149
Gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2021-02-19 14:14:33 -0300

    adicionando novos icones

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,6 +2,11 @@ import React, {useState} from 'react';
 import WindowsButton from '../WindowsButton';
 import {Container, Row, Data} from './styles';
 import logo from '../../images/windows-logo.png';
+import transferIcon from '../../images/transfer.png';
+import helpIcon from '../../images/help.png';
+
+// creditos pelos icones:
+// https://artage.io/en/image-48133
 
 const Footer = () => {
 
@@ -15,6 +20,8 @@ const Footer = () => {
             <Container>
                 <Row>
                     <WindowsButton imagem={logo} texto="Start" onPress={handleStart}/>
+                    <img src={transferIcon}/>
+                    <img src={helpIcon}/>
                     <Data>{data}</Data>
                 </Row>
             </Container>
